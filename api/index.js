3bc2756580fc59b13438b5471c841e5eaedd7f62
Rohit Devhare <rohitdevhare400@gmail.com>
2024-08-14 13:40:51 +0530

    test1

--- api/index.js
+++ api/index.js
@@ -5,4 +5,12 @@ app.get('/api', (req, res) => {
     res.json({ message: 'Hello from Vercel!' });
 });
 
+app.post('/submit', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        return res.status(400).json({ error: 'Name is required' });
+    }
+    res.json({ message: `Hello, ${name}! Your data has been received.` });
+});
+
 module.exports = app;
